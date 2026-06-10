b49c7ae996cac945ee5852dca3c67e9b7eceae18
Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>
2026-06-10 17:15:31 +0200

    updated the config a bit more
    Signed-off-by: Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,24 +41,14 @@ const config: Config = {
     [
       'classic',
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         blog: {
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
+            'https://github.com/jakobht/jakobht.github.io/tree/main',
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
